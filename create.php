67ed8282775c8df1f1519e1e3a5a762709aa63f4
Fairizal <mrzarowtaz@gmail.com.com>
2021-10-30 10:28:56 +0700

    rename file index.php -> create.php

--- create.php
+++ create.php
@@ -5,10 +5,10 @@ $fileName = trim(fgets(STDIN));
 echo "Masukkan Password : ";
 $password = trim(fgets(STDIN));
 
-// $mac = hash_hmac("sha1", $fileName, $password);
 
 if (file_exists($fileName)) {
-	$mac = hash_hmac_file("sha1", $fileName, '12345');
+	// $mac = hash_hmac_file("sha1", $fileName, $password);
+	$mac = hash_hmac("sha1", $fileName, $password);
 	echo "MAC = $mac\n";
 } else {
 	echo "File Tidak Valid\n";
